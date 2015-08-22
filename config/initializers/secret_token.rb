1c114089df2b0a8f2323d115346ee9153b7fef9a
Vincent Chin <vincent.ch91@gmail.com>
2015-08-22 16:15:59 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chessonrails::Application.config.secret_key_base = '1814f3f870405f0fabfae382416be2b0f6b4b6e77362495da03d3291a7887c8ddc51e1908632be99ffefc260d66c3f1958eb1c033e4e1fae3d7253f7d698a06d'
