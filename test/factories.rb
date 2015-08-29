22536477c5d9a2f3245c6485b3a024517b1330ff
Faisal <chaudhary.fsl@gmail.com>
2015-08-29 20:02:45 +0000

    resolve conflit

--- test/factories.rb
+++ test/factories.rb
@@ -1,17 +1,3 @@
-
-FactoryGirl.define do
-
-    factory :piece do
-
-    end
-
-    factory :game do
-        player_one_id "1"
-        player_two_id "2"
-    end
-
-    factory :user do
-
 FactoryGirl.define do
 
    factory :game do
@@ -21,13 +7,10 @@ FactoryGirl.define do
    end
 
     factory :user, aliases:[:player_white,:player_black] do
-
         sequence :email do |n|
             "nguy#{n}@gmail.com"
         end
         password "12345!!!54321"
         password_confirmation "12345!!!54321"
     end
-
-end
 end
