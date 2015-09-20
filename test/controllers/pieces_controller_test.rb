2fd5dcd149175385c1ad416aa7d05f3bb26a81c0
Nehemiah Andrews <nehemiah.andrews@gmail.com>
2015-09-20 21:25:27 +0000

    Moved piece selection test into pieces_controller_test

--- test/controllers/pieces_controller_test.rb
+++ test/controllers/pieces_controller_test.rb
@@ -1,7 +1,13 @@
-require 'test_helper'
-
+require 'test_helper' 
 class PiecesControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  
+  test "select piece success" do 
+    game = FactoryGirl.create(:game)
+    game.populate_board
+    piece = game.pieces.last
+    get :select, :id => piece.id  
+    assert(piece.is_selected?)
+  end
+ 
+
 end
