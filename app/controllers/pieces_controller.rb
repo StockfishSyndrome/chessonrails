931f16e79cf867e59561cc9deb255155907f1745
Vincent Chin <vincent.ch91@gmail.com>
2015-09-21 22:21:21 +0000

    Call valid_move? method on selected piece

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -7,22 +7,21 @@ class PiecesController < ApplicationController
   end
 
   def move
+ 
       selected_piece = Piece.where(is_selected: true, user_id: current_user.id).first
         if selected_piece
             square_id = params[:id].to_i
-            row = square_id / 8
-            col = square_id % 8
+            dest_row = square_id / 8
+            dest_col = square_id % 8
 
-            valid_move = true
-
-            if valid_move
-                selected_piece.row_pos = row
-                selected_piece.col_pos = col
+            if selected_piece.valid_move?(dest_row, dest_col)
+                selected_piece.row_pos = dest_row
+                selected_piece.col_pos = dest_col
                 selected_piece.is_selected = false
                 selected_piece.save
             end
         end
-        redirect_to game_path(selected_piece.game)
+        redirect_to game_path(current_game.id)
   end
 
   private
